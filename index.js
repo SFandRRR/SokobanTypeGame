fafe8e74a0cb76c2db80799e9d1df61d92da797b
Red <85602967+SFandRRR@users.noreply.github.com>
2023-03-03 11:29:22 +0100

    pasowanie propow do tilemap

--- index.js
+++ index.js
@@ -4,115 +4,82 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+const levels =[
+  [
+  ["a","a","b","a","c"],
+  ["b","a","a","b","c"],
+  ["a","b","a","a","c"],
+  ["a","b","a","a","c"],
+  ["a","a","b","a","c"]
+],
+[
+  ["s","s","s","s","s"],
+  ["s","s","s","s","s"],
+  ["s","s","s","s","s"]
+]
+]
 
+class Background extends React.Component{
 
-function Square(props) {
-  return (
-    <button className="square" onClick={props.onClick}>
-      {props.value}
-    </button>
-  );
-}
-
-class Board extends React.Component {
-  constructor(props) {
+  constructor(props){
     super(props);
     this.state = {
-      squares: Array(9).fill(null),
-      xIsNext: true,
-    };
-  }
-
-  handleClick(i) {
-    const squares = this.state.squares.slice();
-    if (calculateWinner(squares) || squares[i]) {
-      return;
+        tile_map: this.props.map
     }
-    squares[i] = this.state.xIsNext ? 'X' : 'O';
-    this.setState({
-      squares: squares,
-      xIsNext: !this.state.xIsNext,
-    });
   }
 
-  renderSquare(i) {
+  renderTile(graphic){
     return (
-      <Square
-        value={this.state.squares[i]}
-        onClick={() => this.handleClick(i)}
-      />
-    );
+      <p>{graphic}</p>
+    )
   }
 
-  render() {
-    const winner = calculateWinner(this.state.squares);
-    let status;
-    if (winner) {
-      status = 'Winner: ' + winner;
-    } else {
-      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+  render(){
+
+    const {tile_map} = this.state;
+    let content = [];
+
+    for(let i in tile_map) {
+      let tile_map_row = [];
+
+      for(let z in tile_map[i]){
+        tile_map_row.push(<th id="tile">{this.renderTile(tile_map[i][z])}</th>);
+      }
+      content.push(<tr>{tile_map_row}</tr>)
     }
 
-    return (
-      <div>
-        <div className="status">{status}</div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
+    return(
+        <div>
+          <table>
+            {content}
+          </table>
         </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
-      </div>
-    );
-  }
+
+    )
+  } 
 }
 
 class Game extends React.Component {
-  render() {
+
+  render(){
     return (
-      <div className="game">
-        <div className="game-board">
-          <Board />
-        </div>
-        <div className="game-info">
-          <div>{/* status */}</div>
-          <ol>{/* TODO */}</ol>
-        </div>
+      <div>
+        <Background id="relative" map = {levels[0]}/>
+        <Background id="absolute" map = {levels[1]}/>
       </div>
-    );
+    )
   }
 }
-
-// ========================================
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<Game />);
+root.render(
+<div id="maingame"><Game /></div>
+);
 
-function calculateWinner(squares) {
-  const lines = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
-  ];
-  for (let i = 0; i < lines.length; i++) {
-    const [a, b, c] = lines[i];
-    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a];
-    }
-  }
-  return null;
-}
+
+
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
