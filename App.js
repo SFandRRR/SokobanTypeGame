0f55d60ab47dc588470e7712ce7e10cd0b1e6248
Red <85602967+SFandRRR@users.noreply.github.com>
2023-03-10 14:57:40 +0100

    Map display with images

--- App.js
+++ App.js
@@ -2,13 +2,58 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './App.css';
 
+import spr_player_s from './graphics/PlayerS.gif'
+import spr_player_n from './graphics/PlayerN.gif'
+import spr_player_w from './graphics/PlayerW.gif'
+import spr_player_e from './graphics/PlayerE.gif'
+
+import spr_empty from './graphics/empty.png'
+
+import spr_ground from './graphics/ground.png'
+import spr_ground_a from './graphics/ground_a.png'
+import spr_ground_b from './graphics/ground_b.png'
+import spr_ground_c from './graphics/ground_c.png'
+
+import spr_border_left from './graphics/border_left.png'
+import spr_border_right from './graphics/border_right.png'
+import spr_border_cavity_vertical from './graphics/border_cavity_vertical.png'
+import spr_border_cavity_vertical_top from './graphics/border_cavity_top.png'
+import spr_border_cavity_vertical_bottom from './graphics/border_cavity_bottom.png'
+
+import spr_border_top from './graphics/border_top.png'
+import spr_border_bottom from './graphics/border_bottom.png'
+import spr_border_bottom_left from './graphics/border_bottom_left.png'
+import spr_border_bottom_right from './graphics/border_bottom_right.png'
+import spr_border_bottom_middle from './graphics/border_bottom_middle.png'
+
+import spr_border_corner_left from './graphics/border_corner_left.png'
+import spr_border_corner_right from './graphics/border_corner_right.png'
+import spr_border_corner_left_bottom from './graphics/border_corner_left_bottom.png'
+import spr_border_corner_right_bottom from './graphics/border_corner_right_bottom.png'
+
+import spr_border_cavity_horizontal from './graphics/border_cavity_horizontal.png'
+import spr_border_cavity_horizontal_left from './graphics/border_cavity_left.png'
+import spr_border_cavity_horizontal_right from './graphics/border_cavity_right.png'
+
+import spr_box_a from './graphics/box_a.png'
+import spr_box_b from './graphics/box_b.png'
+import spr_box_c from './graphics/box_c.png'
+import spr_ladder from './graphics/ladder.png'
+
+
 const levels = [
     [
-        [" ", " ", " ", " ", " "],
-        [" ", "1", " ", " ", " "],
-        [" ", " ", " ", " ", " "],
-        [" ", " ", " ", " ", " "],
-        [" ", " ", " ", " ", " "]
+        ["1", "3", "3", "3", "3", "3", "3", "3", "1"],
+        ["2", "0", "0", "0", "0", "0", "0", "0", "2"],
+        ["2", "0", "2", "0", "3", "3", "3", "0", "2"],
+        ["2", "0", "2", "0", "0", "0", "0", "0", "2"],
+        ["2", "0", "2", "0", "0", "0", "0", "0", "2"],
+        ["2", "0", "0", "0", "3", "3", "3", "0", "2"],
+        ["2", "0", "0", "0", "0", "0", "0", "0", "2"],
+        ["1", "3", "4", "0", "0", "0", "4", "3", "1"],
+        ["1", "1", "2", "0", "0", "0", "2", "3", "1"],
+        ["1", "1", "2", "0", "0", "0", "2", "0", "2"],
+        ["1", "1", "1", "3", "3", "3", "1", "3", "1"],
     ],
     [
         [" ", " ", " ", " ", " "],
@@ -19,6 +64,150 @@ const levels = [
     ]
 ]
 
+
+function tileImage(graphic) {
+    return (<img class="tile_img" src={graphic }/>)
+}
+
+function tileInterpreter(tilemap, type) {
+
+    let new_tilemap = tilemap
+    let maxY = tilemap.length
+
+    if (type == "background") {
+        for (let y in tilemap) {
+            for (let x in tilemap[y]) {
+                let tile = tilemap[y][x]
+
+                if (tile == 4) {
+                    if (tilemap[y][x - 1] == 3 && tilemap[y][(parseInt(x) + 1)] == 0) {
+                        if (tilemap[(parseInt(y) + 1)][x] == 0) {
+                            new_tilemap[y][x] = tileImage(spr_border_corner_right_bottom)
+                        } else {
+                            new_tilemap[y][x] = tileImage(spr_border_corner_right)
+                        }
+
+                    }
+                    if (tilemap[y][x - 1] == 0 && tilemap[y][(parseInt(x) + 1)] == 3) {
+                        if (tilemap[(parseInt(y) + 1)][x] == 0) {
+                            new_tilemap[y][x] = tileImage(spr_border_corner_left_bottom)
+                        } else {
+                            new_tilemap[y][x] = tileImage(spr_border_corner_left)
+                        }
+                    }
+                }
+            }
+        }
+
+        for (let y in tilemap) {
+            for (let x in tilemap[y]) {
+                let tile = tilemap[y][x]
+
+                if (tile == 2) {
+                    if (tilemap[y][(parseInt(x) + 1)] == 0 && tilemap[y][(parseInt(x) - 1)] == 0) {
+
+                        if (tilemap[(parseInt(y) + 1)][x] == 0) {
+                            new_tilemap[y][x] = tileImage(spr_border_cavity_vertical_bottom)
+                        } else
+                            if (tilemap[y - 1][x] == 0) {
+                                new_tilemap[y][x] = tileImage(spr_border_cavity_vertical_top)
+                            } else {
+                                new_tilemap[y][x] = tileImage(spr_border_cavity_vertical)
+                            }
+                    } else {
+                        if (tilemap[y][(parseInt(x) + 1)] == 0) {
+                            new_tilemap[y][x] = tileImage(spr_border_left)
+                        }
+                        if (tilemap[y][x - 1] == 0) {
+                            new_tilemap[y][x] = tileImage(spr_border_right)
+                        }
+                    }
+                    if (tilemap[y][x] == 2) {
+                        new_tilemap[y][x] = tileImage(spr_border_left)
+                    }
+
+                }
+            }
+        }
+
+        for (let y in tilemap) {
+            for (let x in tilemap[y]) {
+                let tile = tilemap[y][x]
+
+                if (tile == 3) {
+                    if (y != 0 && y != (maxY - 1)) {
+                        if (tilemap[(parseInt(y) + 1)][x] == 0 && tilemap[y - 1][x] == 0) {
+
+                            if (tilemap[y][(parseInt(x) + 1)] == 0) {
+                                new_tilemap[y][x] = tileImage(spr_border_cavity_horizontal_right)
+                            } else
+                                if (tilemap[y][x - 1] == 0) {
+                                    new_tilemap[y][x] = tileImage(spr_border_cavity_horizontal_left)
+                                } else { 
+                                    new_tilemap[y][x] = tileImage(spr_border_cavity_horizontal)
+                                }
+                        }
+
+                    }
+                    if (y == 0) {
+                        new_tilemap[y][x] = tileImage(spr_border_top)
+                    }
+
+                    if (y == (maxY - 1)) {
+                        if (tilemap[y][x - 1] == 1 && tilemap[y][(parseInt(x) + 1)] == 1) {
+                            new_tilemap[y][x] = tileImage(spr_border_bottom_middle)
+                        }else
+
+                        if (tilemap[y][x - 1] == 1) {
+                            new_tilemap[y][x] = tileImage(spr_border_bottom_left)
+
+                        }else
+                        if (tilemap[y][(parseInt(x) + 1)] == 1) {
+                            new_tilemap[y][x] = tileImage(spr_border_bottom_right)
+                        } else { 
+                            new_tilemap[y][x] = tileImage(spr_border_bottom)
+                        }
+                    } else if (tilemap[y - 1][x] == 0) {
+                        if (!(tilemap[y - 1][x] == 0)) { new_tilemap[y][x] = tileImage(spr_border_bottom) } else {
+                            if (tilemap[y][x] == 3) {
+                                if (tilemap[y][x - 1] == 1) {
+                                    new_tilemap[y][x] = tileImage(spr_border_bottom_left)
+                                }
+                                if (tilemap[y][(parseInt(x) + 1)] == 1) {
+                                    new_tilemap[y][x] = tileImage(spr_border_bottom_right)
+                                }
+                            }
+                        }
+
+                    } else if (tilemap[(parseInt(y) + 1)][x] == 0) {
+                        new_tilemap[y][x] = tileImage(spr_border_top)
+                    }
+                }
+
+            }
+        }
+
+        for (let y in tilemap) {
+            for (let x in tilemap[y]) {
+                if (tilemap[y][x] == "1") {
+                    new_tilemap[y][x] = tileImage(spr_empty)
+                }
+                if (tilemap[y][x] == "0") {
+                    new_tilemap[y][x] = tileImage(spr_ground)
+                }
+            }
+        }
+                                                          
+    }
+    if (type == "entity") {
+
+    }
+  
+    tilemap = new_tilemap
+    console.log("processed tile map :"+tilemap)
+    return new_tilemap;
+}
+
 class TileMap extends React.Component {
 
     constructor(props) {
@@ -28,14 +217,6 @@ class TileMap extends React.Component {
             move_cmd: this.props.move,
         }
     }
-
-    renderTile(graphic) {
-        return (
-            <p>{graphic}</p>
-        )
-    }
-
-    
 
     move(map, x, y, dir) {
         let tile = map[y][x]
@@ -83,18 +264,20 @@ class TileMap extends React.Component {
 
         let content = [];
 
+        tileInterpreter(tile_map,"background")
+
         for (let i in tile_map) {
             let tile_map_row = [];
 
             for (let z in tile_map[i]) {
-                tile_map_row.push(<th id="tile">{this.renderTile(tile_map[i][z])}</th>);
+                tile_map_row.push(<th >{tile_map[i][z]}</th>);
             }
             content.push(<tr>{tile_map_row}</tr>)
         }
 
         return (
             <div>
-                <table id="tile_table">
+                <table id="tile_table" border="0" cellpadding="0" cellspacing="0">
                     {content}
                 </table>
             </div>
@@ -130,7 +313,7 @@ class Game extends React.Component {
     move_box_cmd = []
 
     checkCollision(collision, x, y, dir) {
-        if (collision[y][x] == 1) {
+        if (collision[y][x] >= 1) {
             console.log("collision : solid")
             return false
         }
@@ -263,15 +446,15 @@ class Game extends React.Component {
         }
 
         //this.move_cmd = []
-
-        return (
+        return (<div><TileMap map={levels[0]} /></div>)
+        /*return (
             <div>
                 <div id="layer0">
-                    {this.state.layer_background}
+                    
                 </div>
 
                 <div id="layer1">
-                    {this.state.layer_entity}
+                    
                 </div>
                 <div id="keypad">
                     <table id="tile_table">
@@ -293,10 +476,10 @@ class Game extends React.Component {
                     </table>
                     {this.state.player_position}
                 </div>
-
+                <img src={spr_player_e} />
 
             </div>
-        )
+        )*/
     }
 }
 
